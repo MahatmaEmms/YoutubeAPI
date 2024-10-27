819bd5180223234392c44663fa55caf5e12cc606
mahat <mahat@DESKTOP-FBDGKUS>
2024-10-26 20:52:15 -0700

    Agregar archivos de proyecto.

---
+++ DAL/SQLDBHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient; //agregamos estos using
+using System.Data; //agregamos estos using
+
+namespace DAL
+{
+    public class SQLDBHelper
+    {
+        public readonly string connectionString = "Data Source=DESKTOP-FBDGKUS\\SERVERMIO;Initial Catalog=Youtube;Integrated Security=True";
+
+        public void GuardarBusqueda(string informacion) // Cambia el parámetro a 'informacion'
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = "INSERT INTO Busquedas (Informacion) VALUES (@informacion)"; // Cambia 'Query' a 'Informacion'
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.Add("@informacion", SqlDbType.NVarChar).Value = informacion; // Cambia 'query' a 'informacion'
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+    }
+}
---
+++ YoutubeAPI/frmYoutube.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DAL;
+using BLL;
+
+namespace YoutubeAPI
+{
+    public partial class frmYoutube : System.Web.UI.Page
+    {
+        protected async void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string query = txtBuscar.Text;
+            ///HEAD
+
+            // Guardar la búsqueda en la base de datos
+            SQLDBHelper busquedasDAL = new SQLDBHelper();
+            busquedasDAL.GuardarBusqueda(query); // Asegúrate de que el método recibe 'query' y lo inserta como 'informacion'
+
+            // 6b43d48803df3bc503f96e4812b56a09cee8f9f7
+            BDYoutube_DAL youtubeDAL = new BDYoutube_DAL();
+            List<BDYoutube_BLL> videos = await youtubeDAL.BuscarVideos(query);
+            rptVideos.DataSource = videos;
+            rptVideos.DataBind();
+        }
+        //HEAD
+
+        //6b43d48803df3bc503f96e4812b56a09cee8f9f7
+    }
+}
